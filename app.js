d39794b95340e8e8a5d340bb9b035cf98a70db7d
thatkyle <thatkyle@gmail.com>
2018-08-07 15:48:11 -0600

    Updated app.js
    Fixed program to get and render searched user's name, avatar, and dms.

--- app.js
+++ app.js
@@ -27,48 +27,62 @@ const twitPromise = twitArgs => {
 // Accepts a user_id and returns an object whose values hold request params
 // [path, {options}], which can either be passed directly into twitPromise, or
 // passed into twit.get as its first two parameters using the spread operator
-const twitRequests = user_id => ({
+const twitRequestsId = user_id => ({
   tweets: [`statuses/user_timeline`, {user_id, count: 5}],
   friends: [`friends/list`, {user_id, skip_status: true, include_user_entities: false, cursor: -1}],
   dms: [`direct_messages/events/list`, {user_id}],
   userprofile: [`/users/show`, {user_id}],
 });
 
+const twitRequestsName = screen_name => ({
+  tweets: [`statuses/user_timeline`, {screen_name, count: 5}],
+  friends: [`friends/list`, {screen_name, skip_status: true, include_user_entities: false, cursor: -1}],
+  dms: [`direct_messages/events/list`, {screen_name}],
+  userprofile: [`/users/show`, {screen_name}],
+});
+
 // Accepts a userId which is passed to twitRequests, then iterates through all
 // requests in twitRequests and returns an object holding the raw responses of
 // both successful and failed attempts for further processing
 const getTwitRequests = async function(userId) {
-  const twitsData = {};
-  for (const [key, twitArgs] of Object.entries(twitRequests(userId))) {
+  const twitsDataById = {};
+  for (const [key, twitArgs] of Object.entries(twitRequestsId(userId))) {
     await twitPromise(twitArgs)
-      .catch(err => {twitsData[key] = err})
-      .then(data => {twitsData[key] = data})
+      .catch(err => {twitsDataById[key] = err})
+      .then(data => {twitsDataById[key] = data})
   }
-  return(twitsData);
+  const twitsDataByName = {};
+  for (const [key, twitArgs] of Object.entries(twitRequestsName(twitsDataById.tweets[0].user.screen_name))) {
+    await twitPromise(twitArgs)
+      .catch(err => {twitsDataByName[key] = err})
+      .then(data => {twitsDataByName[key] = data})
+  }
+  return([twitsDataById,twitsDataByName]);
 }
 
 app.get('/', (req, res) => {
   +async function(userId = defaultUserId) {
+    renderData = await getTwitRequests(userId);
     let dms, tweets, friends, userprofile;
-    renderData = await getTwitRequests(userId)
-    if (! renderData.userprofile.errors || renderData.userprofile !== undefined) {
-      userprofile = renderData.userprofile;
+    if (! renderData[0].userprofile.errors || renderData[0].userprofile !== undefined) {
+      userprofile = renderData[0].userprofile;
     }
-    if (! renderData.tweets.errors || renderData.tweets !== undefined) {
-      tweets = renderData.tweets;
+    if (! renderData[0].tweets.errors || renderData[0].tweets !== undefined) {
+      tweets = renderData[0].tweets;
     }
-    if (! renderData.friends.errors || renderData.friends !== undefined) {
-      users = renderData.friends;
+    if (! renderData[0].friends.errors || renderData[0].friends !== undefined) {
+      users = renderData[0].friends;
     }
-    if (! renderData.dms.errors || renderData.dms !== undefined) {
-      dms = renderData.dms;
+    if (! renderData[0].dms.errors || renderData[0].dms !== undefined) {
+      dms = renderData[0].dms;
     }
+    userprofile = renderData[1].userprofile;
     res.render('layout', {
       profileName: userprofile.name,
       profileScreenName: userprofile.screen_name,
       profileImageUrl: userprofile.profile_image_url,
       tweets,
-      users: renderData.friends.users,
+      users: renderData[0].friends.users,
       dms
     })
   }()
